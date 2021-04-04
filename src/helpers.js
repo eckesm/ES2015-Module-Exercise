316fa1053c2b964a66e4f2f3d535887bcd6840cc
Matthew Eckes <eckesm@gmail.com>
2021-04-04 14:30:31 -0400

    Incorporporating suggested solutions.

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,12 @@
 const choice = items => items[Math.floor(Math.random() * items.length)];
 
-const remove = (items, item) => items.splice(items.indexOf(item),1);
+// const remove = (items, item) => items.splice(items.indexOf(item),1);
+function remove(items, item) {
+	for (let i = 0; i < items.length; i++) {
+		if (items[i] === item) {
+			return [ ...items.slice(0, i), ...items.slice(i + 1) ];
+		}
+	}
+}
 
-export {choice, remove}
+export { choice, remove };
--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ let fruit = choice(fruits);
 console.log(`I'd like one ${fruit}, please.`);
 console.log(`Here you go: ${fruit}`);
 console.log(`Delicious!  May I have another?`);
-remove(fruits, fruit);
-console.log(`I'm sorry, we're all out.  We have ${fruits.length} left.`);
+let remaining = remove(fruits, fruit);
+console.log(`I'm sorry, we're all out.  We have ${remaining.length} left.`);
